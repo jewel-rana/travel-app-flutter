35e96dc0d98bb4c02bc11bd4c8b4de63d7b8fb43
Jewel Rana <jewelrana.dev@gmail.com>
2024-03-18 21:08:16 +0600

    Keyboard overflow on form bug fix

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -19,135 +19,143 @@ class _LoginState extends State<Login> {
     double h = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            width: w,
-            height: h * .3,
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("img/welcome-three.jpg"),
-                    fit: BoxFit.cover)),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20),
-            width: w,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AppLargeText(text: "Hello"),
-                AppText(text: "Sign into your account"),
-                const SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 10,
-                            spreadRadius: 7,
-                            offset: Offset(1, 1),
-                            color: Colors.grey.withOpacity(0.2))
-                      ]),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.only(left: 10, right: 10),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                              width: 1.0,
-                            )),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                              width: 1.0,
-                            )),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30))),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 10,
-                            spreadRadius: 7,
-                            offset: Offset(1, 1),
-                            color: Colors.grey.withOpacity(0.2))
-                      ]),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                              width: 1.0,
-                            )),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                              width: 1.0,
-                            )),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30))),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Expanded(child: Container()),
-                    AppText(text: "Forgot your password?"),
-                  ],
-                )
-              ],
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Container(
+              width: w,
+              height: h * .25,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("img/welcome-three.jpg"),
+                      fit: BoxFit.cover)),
             ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Button(
-            width: 100,
-            color: Colors.white,
-            backgroundColor: AppColors.mainColor,
-            borderColor: AppColors.mainColor,
-            isIcon: false,
-            text: "Sign in",
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          RichText(
-              text: TextSpan(
-                  text: "Don\'t have an account?",
-                  style: TextStyle(color: Colors.grey[500], fontSize: 20),
-                  children: const [
-                    TextSpan(
-                      text: " Create",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.black
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              width: w,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AppLargeText(text: "Hello"),
+                  AppText(text: "Sign into your account"),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 7,
+                              offset: Offset(1, 1),
+                              color: Colors.grey.withOpacity(0.2))
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Email address",
+                          prefixIcon: const Icon(Icons.email, color: AppColors.mainColor,),
+                          contentPadding: const EdgeInsets.only(left: 10, right: 10),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                                width: 1.0,
+                              )),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                                width: 1.0,
+                              )),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 10,
+                              spreadRadius: 7,
+                              offset: Offset(1, 1),
+                              color: Colors.grey.withOpacity(0.2))
+                        ]),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                          prefixIcon: const Icon(Icons.password, color: AppColors.mainColor,),
+                          contentPadding: const EdgeInsets.only(left: 10, right: 10),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                                width: 1.0,
+                              )),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                                width: 1.0,
+                              )),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(child: Container()),
+                      AppText(text: "Forgot your password?"),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Button(
+              width: 100,
+              color: Colors.white,
+              backgroundColor: AppColors.mainColor,
+              borderColor: AppColors.mainColor,
+              isIcon: false,
+              text: "Sign in",
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            RichText(
+                text: TextSpan(
+                    text: "Don\'t have an account?",
+                    style: TextStyle(color: Colors.grey[500], fontSize: 20),
+                    children: const [
+                      TextSpan(
+                        text: " Create",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black
+                        )
                       )
-                    )
-                  ])),
-          const SizedBox(
-            height: 40,
-          ),
-        ],
+                    ])),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
